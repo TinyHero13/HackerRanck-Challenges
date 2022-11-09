2069c263a1808ead1a8fc555448e0bba36bee9db
Yasmim <42361925+TinyHero13@users.noreply.github.com>
2022-11-09 10:57:46 -0300

    Delete PreparationKit directory

--- PreparationKit/Mini-MaxSum.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python3
-def miniMaxSum(arr):
-    arr = sorted(arr)
-    print(sum(arr[:-1]), sum(arr[1:]))
-
-if __name__ == '__main__':
-    arr = list(map(int, input().rstrip().split()))
-    miniMaxSum(arr)

dd5897055d1cb674713282d14721b7ebbaa3a4cf
Yasmim <42361925+TinyHero13@users.noreply.github.com>
2022-11-09 15:48:55 -0300

    Update SparseArrays.py

--- SparseArrays.py
+++ SparseArrays.py
@@ -1,14 +1,5 @@
-strings = ['aba', 'baba', 'aba', 'xzxb']
-queries = ['aba', 'xzxb', 'ab']
-
-arr, result = [], []
-for i in strings:
-    if(i in queries):
-       arr.append(i)
-for i in queries:
-    if(i in arr):
-       result.append(arr.count(i))
-    elif(i not in strings):
-       result.append(0)
-
-print(result)
+def matchingStrings(strings, queries):
+    lst = []
+    for i in queries:
+        lst.append(strings.count(i))
+    return lst   

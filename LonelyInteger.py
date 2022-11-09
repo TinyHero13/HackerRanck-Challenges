7162a9946c15f78775d74be0bdf2cd4930ceb067
Yasmim <42361925+TinyHero13@users.noreply.github.com>
2022-11-09 11:08:29 -0300

    Create LonelyInteger.py

---
+++ LonelyInteger.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+import os
+
+def lonelyinteger(a):
+    for i in a:
+        if(a.count(i) == 1):
+            unique = i
+    return unique
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    n = int(input().strip())
+    a = list(map(int, input().rstrip().split()))
+    result = lonelyinteger(a)
+    fptr.write(str(result) + '\n')
+    fptr.close()

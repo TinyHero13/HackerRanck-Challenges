04d7c42fa3c24428c7a2866fce68dd30683310f8
Yasmim <42361925+TinyHero13@users.noreply.github.com>
2022-11-09 10:57:31 -0300

    Update Mini-MaxSum.py

--- Mini-MaxSum.py
+++ Mini-MaxSum.py
@@ -1,7 +1,7 @@
 #!/bin/python3
-
 def miniMaxSum(arr):
-    print(sum(arr) - max(arr), sum(arr) - min(arr))
+    arr = sorted(arr)
+    print(sum(arr[:-1]), sum(arr[1:]))
 
 if __name__ == '__main__':
     arr = list(map(int, input().rstrip().split()))
